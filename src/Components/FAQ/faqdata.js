e53fd3ec8e488273d490d5b65092736baf6d1082
Rohan Jha <rohan21_ug@ece.nits.ac.in>
2023-04-07 21:53:59 +0530

    add:about us and faq section added

---
+++ src/Components/FAQ/faqdata.js
@@ -0,0 +1,26 @@
+export const Data = [
+  {
+    Id: 1,
+    Question: " What is ECS?",
+    Answer:
+      "ECS is the technical society of ECE which conducts various events, workshops and contests for students in and out of the department. It is responsible for all the events related to ECE.",
+  },
+  {
+    Id: 2,
+
+    Question: " In which year can you expect companies coming for internships?",
+    Answer: "3rd Year.",
+  },
+  {
+    Id: 3,
+
+    Question: " What are the events conducted by your branch society (ECS) ?",
+    Answer: [
+      "ORIENTATION(It is an introductory session for the freshers to interact with the branch members ),",
+      " UTKRISHTHA( Utkrishtha is the annual freshers event for the ECE department. ),",
+      " SPECTRUM (Spectrum is the technical weekend of ECS which is conducted annually),",
+      " SPECTRUM CONNECT ( Spectrum Connect was conducted in the online mode to connect students in and out of the department despite being physically miles away.),",
+      " DEHLEEZ (Farewell)",
+    ],
+  },
+];
