9b976d81465547ff1d935e2e481401933caa17d7
Rohan Jha <rohan21_ug@ece.nits.ac.in>
2023-04-07 16:59:43 +0530

    add:get connected system added

--- src/Components/Connection/images.js
+++ src/Components/Connection/images.js
@@ -2,30 +2,32 @@ const images = [
   {
     id: 1,
     src: "https://res.cloudinary.com/dt3o8ldp9/image/upload/q_30/w_480,h_480,c_fit/v1680619576/oikyotaan/DSC_0211_pxwqk2.webp",
+    name: "Rohan Jha",
+    designation: "CEO",
   },
   {
     id: 2,
     src: "https://res.cloudinary.com/dt3o8ldp9/image/upload/q_30/w_480,h_480,c_fit/v1680619576/oikyotaan/DSC_0211_pxwqk2.webp",
+    name: "Rohan Jha",
+    designation: "CEO",
   },
   {
     id: 3,
     src: "https://res.cloudinary.com/dt3o8ldp9/image/upload/q_30/w_480,h_480,c_fit/v1680619576/oikyotaan/DSC_0211_pxwqk2.webp",
+    name: "Rohan Jha",
+    designation: "CEO",
   },
   {
     id: 4,
     src: "https://res.cloudinary.com/dt3o8ldp9/image/upload/q_30/w_480,h_480,c_fit/v1680619576/oikyotaan/DSC_0211_pxwqk2.webp",
+    name: "Rohan Jha",
+    designation: "CEO",
   },
   {
     id: 5,
     src: "https://res.cloudinary.com/dt3o8ldp9/image/upload/q_30/w_480,h_480,c_fit/v1680619576/oikyotaan/DSC_0211_pxwqk2.webp",
-  },
-  {
-    id: 6,
-    src: "https://res.cloudinary.com/dt3o8ldp9/image/upload/q_30/w_480,h_480,c_fit/v1680619576/oikyotaan/DSC_0211_pxwqk2.webp",
-  },
-  {
-    id: 7,
-    src: "https://res.cloudinary.com/dt3o8ldp9/image/upload/q_30/w_480,h_480,c_fit/v1680619576/oikyotaan/DSC_0211_pxwqk2.webp",
+    name: "Rohan Jha",
+    designation: "CEO",
   },
 ];
 export default images;
